1fdb0acc6f496d069533172b4de0e61d07f743f7
Jesse Pinho <jesse@jessepinho.com>
2014-01-28 21:05:44 -0600

    Use the relations table to connect words

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,2 +1,4 @@
 class Word < ActiveRecord::Base
+  has_and_belongs_to_many :related_words, class_name: 'Word', foreign_key: 'related_id', association_foreign_key: 'relating_id', join_table: :relations
+  has_and_belongs_to_many :relating_words, class_name: 'Word', foreign_key: 'relating_id', association_foreign_key: 'related_id', join_table: :relations
 end
