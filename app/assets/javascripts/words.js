3329fb3a48dcc2d364066d0d6cbbc01cbc8a8cac
Jesse Pinho <jesse@jessepinho.com>
2014-02-01 13:44:06 -0600

    Clean up comments and code

--- app/assets/javascripts/words.js
+++ app/assets/javascripts/words.js
@@ -5,7 +5,6 @@ var Renderer = function(canvas){
 
   var that = {
     init:function(system){
-      //
       // the particle system will call the init function once, right before the
       // first frame is to be drawn. it's a good place to set up the canvas and
       // to pass the canvas size to the particle system
@@ -17,22 +16,20 @@ var Renderer = function(canvas){
       // if the canvas is ever resized, screenSize should be called again with
       // the new dimensions
       particleSystem.screenSize(canvas.width, canvas.height)
-      //particleSystem.screenPadding(80) // leave an extra 80px of whitespace per side
 
       // set up some event handlers to allow for node-dragging
       that.initMouseHandling()
     },
 
     redraw:function(){
-      //
-      // redraw will be called repeatedly during the run whenever the node positions
-      // change. the new positions for the nodes can be accessed by looking at the
-      // .p attribute of a given node. however the p.x & p.y values are in the coordinates
-      // of the particle system rather than the screen. you can either map them to
-      // the screen yourself, or use the convenience iterators .eachNode (and .eachEdge)
-      // which allow you to step through the actual node objects but also pass an
-      // x,y point in the screen's coordinate system
-      //
+      // redraw will be called repeatedly during the run whenever the node
+      // positions change. the new positions for the nodes can be accessed by
+      // looking at the .p attribute of a given node. however the p.x & p.y
+      // values are in the coordinates of the particle system rather than the
+      // screen. you can either map them to the screen yourself, or use the
+      // convenience iterators .eachNode (and .eachEdge) which allow you to step
+      // through the actual node objects but also pass an x,y point in the
+      // screen's coordinate system
       ctx.clearRect(0,0, canvas.width, canvas.height)
 
       particleSystem.eachEdge(function(edge, pt1, pt2){
@@ -54,14 +51,8 @@ var Renderer = function(canvas){
       particleSystem.eachNode(function(node, pt){
         // node: {mass:#, p:{x,y}, name:"", data:{}}
         // pt:   {x:#, y:#}  node position in screen coords
-
         ctx.font = '30px/45px Cardo, serif';
         ctx.textAlign = 'center';
-        var w = ctx.measureText(node.data.text).width;
-        ctx.fillStyle = 'white';
-        //ctx.arc(pt.x, pt.y, 10, 0, Math.PI*2);
-        //ctx.fill();
-        ctx.closePath();
         ctx.fillStyle = 'black';
         ctx.fillText(node.data.text, pt.x, pt.y);
       })
