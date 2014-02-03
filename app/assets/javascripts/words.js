533e5cbdb99fff94c713f62c4e4fa36efcb98245
Jesse Pinho <jesse@jessepinho.com>
2014-02-03 07:23:18 -0600

    Rename clicked to mousedown

--- app/assets/javascripts/words.js
+++ app/assets/javascripts/words.js
@@ -99,7 +99,7 @@ var Renderer = function(canvas){
       // set up a handler object that will initially listen for mousedowns then
       // for moves and mouseups while dragging
       var handler = {
-        clicked:function(e){
+        mousedown:function(e){
           var pos = $(canvas).offset();
           _mouseP = arbor.Point(e.pageX-pos.left, e.pageY-pos.top)
           dragged = particleSystem.nearest(_mouseP);
@@ -139,8 +139,7 @@ var Renderer = function(canvas){
       }
 
       // start listening
-      $(canvas).mousedown(handler.clicked);
-
+      $(canvas).mousedown(handler.mousedown);
     },
 
   }
