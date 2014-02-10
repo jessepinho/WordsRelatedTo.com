b2591ef331c3e4c6aa70f694a2cbfd093cd0aafc
Jesse Pinho <jesse@jessepinho.com>
2014-02-10 08:38:03 -0600

    Reverse "Enable asset compilation"

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,6 @@ WordsRelatedTo::Application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-  config.assets.enabled = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
