5d655a014002a2a7531db9c27a3bef72aa530c66
Jesse Pinho <jesse@jessepinho.com>
2014-02-10 07:40:41 -0600

    Fall back to assets pipeline

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ WordsRelatedTo::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
   config.assets.enabled = true
 
   # Generate digests for assets URLs.
