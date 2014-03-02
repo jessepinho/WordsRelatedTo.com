8b3a974530ee5c5e09061f141b053fdccfbda79f
Jesse Pinho <jesse@jessepinho.com>
2014-03-02 02:42:33 -0600

    Check for the existence of a word graph before animating it

--- public/javascripts/words.js
+++ public/javascripts/words.js
@@ -124,25 +124,27 @@ var Renderer = function(canvas){
 }
 
 $(document).ready(function(){
-  var sys = arbor.ParticleSystem(1000, 600, 0.5);
-  sys.parameters({gravity:true});
-  sys.renderer = Renderer(".word-graph");
+  if ($('.word-graph').length) {
+    var sys = arbor.ParticleSystem(1000, 600, 0.5);
+    sys.parameters({gravity:true});
+    sys.renderer = Renderer(".word-graph");
 
-  var wordId = $('.word-graph h1 .word').data('word-id');
-  sys.addNode(wordId, {
-    element: $('.word-graph h1 .word')
-  });
-  $('.word-graph .word:not([data-word-id=' + wordId + '])').each(function(i) {
-    var relatedWordId = $(this).data('word-id');
-    sys.addNode(relatedWordId, {
-      element: $(this)
+    var wordId = $('.word-graph h1 .word').data('word-id');
+    sys.addNode(wordId, {
+      element: $('.word-graph h1 .word')
     });
-    sys.addEdge(wordId, relatedWordId);
-  });
+    $('.word-graph .word:not([data-word-id=' + wordId + '])').each(function(i) {
+      var relatedWordId = $(this).data('word-id');
+      sys.addNode(relatedWordId, {
+        element: $(this)
+      });
+      sys.addEdge(wordId, relatedWordId);
+    });
 
-  window.arborParticleSystem = sys;
+    window.arborParticleSystem = sys;
 
-  $('.word.add').click(function (e) {
-    $('input[type="text"]', this).focus();
-  });
+    $('.word.add').click(function (e) {
+      $('input[type="text"]', this).focus();
+    });
+  }
 })
